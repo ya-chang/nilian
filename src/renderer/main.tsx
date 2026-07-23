2153c2590466d4418f0a3dc969a6c7df49ef189b
yac <3117797385@qq.com>
2026-07-23 12:17:39 +0800

    Update src/renderer/main.tsx

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -3,9 +3,13 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { installIPCShim } from './ipc-shim'
 import App from './App'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import './styles/global.css'
+
+// 安装 IPC 模拟层（替代 Electron 主进程通信）
+installIPCShim()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

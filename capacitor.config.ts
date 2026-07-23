9703584ccf701e8e11ee01f5b636295261d57e92
yac <3117797385@qq.com>
2026-07-23 12:14:34 +0800

    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nilian.app',
+  appName: '拟恋',
+  webDir: 'dist'
+};
+
+export default config;
